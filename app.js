bd96362f60f3aa82d6ec475d9289ce3ae011ed97
gemelodyyu <yuge.chang0512@gmail.com>
2020-11-02 20:27:56 -0600

    final update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Use the D3 library to read in samples.json.
 d3.json("data/samples.json").then((importData) => {
 	console.debug(importData);
-	var data = importData[0];
+	var data = importData;
 
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 	// get all the names/IDs and add them as options in the dropdown menu
